ec0d579f8fa81f25884181c661f4a1c1ab506acb
happylifeeveryday <yihanshang0@gmail.com>
2022-11-16 21:49:08 +0800

    初步完成剪刀石头布类的构造

---
+++ Rock-paper-scissors/struct.py
@@ -0,0 +1,14 @@
+import random
+class rock():
+    position_x=random.random()
+    position_y=random.random()
+
+class paper():
+    position=0
+
+class scissors():
+    position=0
+
+rock0=rock()
+print(rock0.position_x)
+print(rock0.position_y)
